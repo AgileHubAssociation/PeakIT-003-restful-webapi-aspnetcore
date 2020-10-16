2e8a466882826d65a7f425a4dc9cf55769fd53b5
Asavei Florin <asaveiflorin@gmail.com>
2020-10-16 17:32:44 +0300

    added controllers with simple actions

---
+++ BackEnd/LearningQ/LearningQ.API/Controllers/QueueController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningQ.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/queue")]
+    public class QueueController : ControllerBase
+    {
+
+        // api/queue
+        [HttpGet]
+        public ActionResult GetQueues()
+        {
+            return Ok("all the queues");
+        }
+
+        // api/queue/5
+        [HttpGet("{queueId}")]
+        public ActionResult GetQueue(int queueId)
+        {
+            return Ok($"queue with id {queueId}");
+        }
+
+        // api/queue/
+        [HttpPost]
+        public ActionResult CreateQueue(object queue) //TODO: return created entity
+        {
+            return NoContent();
+        }
+
+        // api/queue/5
+        [HttpPut("{queueId}")]
+        public ActionResult UpdateQueue(int queueId, object queue)
+        {
+            return NoContent();
+        }
+
+        // api/queue/5/items
+        [HttpPut("{queueId}/includeItems")]
+        public ActionResult UpdateQueueWithItems([FromRoute] int queueId, object queue)
+        {
+            return NoContent();
+        }
+
+        //TODO: implement PATCH
+
+        // api/queue/5
+        [HttpDelete("{queueId}")]
+        public ActionResult DeleteQueue(int queueId)
+        {
+            return NoContent();
+        }
+
+    }
+}
---
+++ BackEnd/LearningQ/LearningQ.API/Controllers/QueueItemController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningQ.API.Controllers
+{
+    [ApiController]
+    [Route("api/queue/{queueId}/item")]
+    public class ItemController : ControllerBase
+    {
+
+        // api/queue/5/item
+        [HttpGet]
+        public ActionResult GetItems(int queueId)
+        {
+            return Ok($"all item from queue {queueId}");
+        }
+
+        // api/queue/5/item/7 
+        [HttpGet("{itemId}")]
+        public ActionResult GetItem(int queueId, int itemId)
+        {
+            return Ok($"item {itemId} from queue {queueId}");
+        }
+
+        // api/queue/5/item/
+        [HttpPost]
+        public ActionResult CreateItem(int queueId, object item) //TODO: return created entity
+        {
+            return NoContent();
+        }
+
+        // api/queue/5/item/7 
+        [HttpPut("{itemId}")]
+        public ActionResult UpdateItem(int queueId, int itemId, object item)
+        {
+            return NoContent();
+        }
+
+        //TODO: implement PATCH
+
+        // api/queue/5/item/7
+        [HttpDelete("{itemId}")]
+        public ActionResult DeleteQueue(int queueId, int itemId)
+        {
+            return NoContent();
+        }
+
+    }
+}
