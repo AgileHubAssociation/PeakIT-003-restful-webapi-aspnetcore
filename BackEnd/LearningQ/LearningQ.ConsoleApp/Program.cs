2f12d395b39cd8caf65970e290a0049f83be046a
Asavei Florin <asaveiflorin@gmail.com>
2020-10-17 19:50:15 +0300

    renamed project

---
+++ BackEnd/LearningQ/LearningQ.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningQ.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- BackEnd/LearningQ/LearningQ.Simple/Startup.cs
+++ BackEnd/LearningQ/LearningQ.Simple/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace LearninQ.Simple
+namespace LearningQ.Simple
 {
     public class Startup
     {
