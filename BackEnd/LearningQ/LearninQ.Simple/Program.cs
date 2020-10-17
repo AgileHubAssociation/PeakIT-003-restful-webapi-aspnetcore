d0f3231641c97c26b87fa010544aa528bdb5ef26
Asavei Florin <asaveiflorin@gmail.com>
2020-10-17 13:19:44 +0300

    recreating projects

--- BackEnd/LearningQ/LearninQ.Simple/Program.cs
+++ BackEnd/LearningQ/LearninQ.Simple/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace LearningQ.API
+namespace LearninQ.Simple
 {
     public class Program
     {
